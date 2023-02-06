5c1b1ee37bf1efaba7c833485a33aa4058e8cd54
Sloan Nelson <sloan.nelson17@gmail.com>
2023-02-06 16:24:57 -0700

    Calculator Works Now

--- Mission4/wwwroot/js/calculator.js
+++ Mission4/wwwroot/js/calculator.js
@@ -1,6 +1,7 @@
 ﻿
 document.getElementById("btnSend").addEventListener("click", function () {
 
+
     var grade1 = parseFloat(document.getElementById("A").value * 0.5);
     var grade2 = parseFloat(document.getElementById("G").value * 0.1);
     var grade3 = parseFloat(document.getElementById("Q").value * 0.1);
@@ -8,49 +9,48 @@ document.getElementById("btnSend").addEventListener("click", function () {
     var grade5 = parseFloat(document.getElementById("F").value * 0.1);
     var grade6 = parseFloat(document.getElementById("I").value * 0.1);
 
-    finalGrade = grade1 + grade2 + grade3 + grade4 + grade5 + grade6
+    finalgrade = grade1 + grade2 + grade3 + grade4 + grade5 + grade6
 
-    if (finalgrade.value > 94) {
+    if (finalgrade > 94) {
         alert("A")
     }
-    else if (finalgrade.value >= 90) {
+    else if (finalgrade >= 90) {
         alert("A-")
     }
-    else if (finalgrade.value >=87 ) {
+    else if (finalgrade >= 87) {
         alert("B+")
     }
-    else if (finalgrade.value >= 84) {
+    else if (finalgrade >= 84) {
         alert("B")
     }
-    else if (finalgrade.value >= 80) {
+    else if (finalgrade >= 80) {
         alert("B-")
     }
-    else if (finalgrade.value >= 77) {
+    else if (finalgrade >= 77) {
         alert("C+")
     }
-    else if (finalgrade.value >= 74) {
+    else if (finalgrade >= 74) {
         alert("C")
     }
-    else if (finalgrade.value >= 70) {
+    else if (finalgrade >= 70) {
         alert("C-")
     }
-    else if (finalgrade.value >= 67) {
+    else if (finalgrade >= 67) {
         alert("D+")
     }
-    else if (finalgrade.value >= 64) {
+    else if (finalgrade >= 64) {
         alert("D")
     }
-    else if (finalgrade.value >= 60) {
+    else if (finalgrade >= 60) {
         alert("D-")
     }
-    else if (finalgrade.value <60) {
+    else if (finalgrade < 60) {
         alert("E")
     }
-
     
 
-    
+})
 
 
 
-}
+
