f426ec62a5413dc8ae9dba503f2a0d7a95ad7144
Sloan Nelson <sloan.nelson17@gmail.com>
2023-02-06 19:40:13 -0700

    Mission 5

---
+++ Mission4/Controllers/BlahController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult calculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult calculator(calculatorModel model)
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Mission4/Models/calculatorModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+// This is the model for my calculator. asp.net allows me to get and set everything at once. I am using system.ComponentModel to set a range from 0-100 
+namespace Mission4.Models
+{
+    public class calculatorModel
+    {
+        [Range(0, 100, ErrorMessage ="Value must be between 0 and 100")]
+        public int A { get; set; }
+        [Range(0, 100, ErrorMessage = "Value must be between 0 and 100")]
+        public int G { get; set; }
+        [Range(0, 100, ErrorMessage = "Value must be between 0 and 100")]
+        public int Q { get; set; }
+        [Range(0, 100, ErrorMessage = "Value must be between 0 and 100")]
+        public int M { get; set; }
+        [Range(0, 100, ErrorMessage = "Value must be between 0 and 100")]
+        public int F { get; set; }
+        [Range(0, 100, ErrorMessage = "Value must be between 0 and 100")]
+        public int I { get; set; }
+
+
+    }
+}
